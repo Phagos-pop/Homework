ab08e602eea6c24d042df080f5bd915291505b4f
Ilya Tzvirko <zaroxsino@gmail.com>
2021-06-18 15:04:52 +0300

    сделал все задания

---
+++ HW_02_task_4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HW_02_task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte a1 = 1;
+            System.SByte b1 = 1;
+            short a2 = 1;
+            System.Int16 b2 = 1;
+            int a3 = 1;
+            System.Int32 b3 = 1;
+            long a4 = 1;
+            System.Int64 b4 = 1;
+            byte a5 = 1;
+            System.Byte b5 = 1;
+            ushort a6 = 1;
+            System.UInt16 b6 = 1;
+            char a7 = '1';
+            System.Char b7 = '1';
+            uint a8 = 1;
+            System.UInt32 b8 = 1;
+            ulong a9 = 1;
+            System.UInt64 b9 = 1;
+            float a10 = 1;
+            System.Single b10 = 1;
+            double a11 = 1;
+            System.Double b11 = 1;
+            decimal a12 = 1;
+            System.Decimal b12 = 1;
+            object a13 = 1;
+            System.Object b13 = 1;
+            string a14 = "1";
+            System.String b14 = "1";
+        }
+    }
+}
