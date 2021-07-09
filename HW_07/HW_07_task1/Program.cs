cd2755e82ec7b0aa7d2e214f1db172a28f070065
Ilya Tzvirko <zaroxsino@gmail.com>
2021-07-09 17:44:41 +0300

    Task 1

---
+++ HW_07/HW_07_task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW_07_task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentAndTeacherTest.CreatePersonAndSyaHello();
+            StudentAndTeacherTest.CreateStudentAndSetAge();
+            StudentAndTeacherTest.CreateTeaherAndExplain();
+        }
+    }
+}
