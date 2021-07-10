b6764135c343d0f2c66369d5520fb146eb6d86ec
Ilya Tzvirko <zaroxsino@gmail.com>
2021-07-10 15:11:00 +0300

    Task 2

---
+++ HW_07/HW_07_task2/House.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_07_task2
+{
+    class House
+    {
+        public int area { set; get; }
+
+        public House(int _area)
+        {
+            area = _area;
+        }
+        public void ShowData()
+        {
+            Console.WriteLine($"I am a house, my area is {area} m2");
+        }
+        public void GetDoor()
+        {
+            Door door = new Door("Red");
+            door.ShowData();
+        }
+    }
+}
